f15def6f4a1800c32afde54109d22d112f30f5b5
Axel Dadure <keyloags@gmail.com>
2020-04-18 14:54:09 +0200

    hid the api key

--- src/data/bookServices.js
+++ src/data/bookServices.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import {key} from './key'
 
 export const getBooks = () => {
     return (
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=quilting&key=AIzaSyD-Q1Hb8BkVki6_rUNkRKolHbGxP9N7m50`)
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=quilting&key=${key}`)
         .then(res => {
             return res.data;
         }).catch(function(error) {
